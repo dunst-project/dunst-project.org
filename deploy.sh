4170391f06dc9a88bcd64d239180a76486173f56
fwsmit <fw.smit01@gmail.com>
2022-02-24 13:01:08 +0100

    Add push option to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -e
 
+usage()
+{
+	echo "Usage:"
+	echo "$(basename $0) [-p]"
+	echo "-p: push the changes to the gh-pages and master branches"
+}
+
+declare PUSH
+
+while getopts 'hp' c
+do
+	case $c in
+		h) usage; exit ;;
+		p) PUSH=1 ;;
+	esac
+done
+
 if [ ! -d "public/" ]; then
 	git worktree add public/ gh-pages
 fi
@@ -10,3 +27,9 @@ COMMIT="$(git rev-parse HEAD | cut -b1-7)"
 cd public/
 git add -A
 git commit -m "Deploy $COMMIT"
+
+if [ ! -z $PUSH ]; then
+	git push
+	cd ..
+	git push
+fi
